c332005855ee902afe9251035e6f2ad3f3a97771
Ankit patel <102891012+ankit8821@users.noreply.github.com>
2024-02-23 13:21:10 +0530

    Update program1.js

--- program1.js
+++ program1.js
@@ -2,10 +2,36 @@
  * @param {string} s
  * @return {boolean}
  */
-var isValid = function(s) {
-    
+
+   var isValid = function(s) {
+    const stack = [];
+    const pairs = {
+        '(': ')',
+        '{': '}',
+        '[': ']'
+    };
+
+    for (let i = 0; i < s.length; i++) {
+        const char = s[i];
+        if (char in pairs) {
+            stack.push(char);
+        } else {
+            const top = stack.pop();
+            if (!top || pairs[top] !== char) {
+                return false;
+            }
+        }
+    }
+
+    return stack.length === 0;
 };
 
 module.exports = { isValid };
+
+
+const { isValid } = require('./yourModuleFileName');
+console.log(isValid("()")); 
+console.log(isValid("()[]{}"));
+console.log(isValid("(]"));
 
 
