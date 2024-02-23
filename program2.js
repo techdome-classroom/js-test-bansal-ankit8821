0c2e93cf8dbd7730fa6d3c814046fa1f826de343
Ankit patel <102891012+ankit8821@users.noreply.github.com>
2024-02-23 13:25:13 +0530

    Update program2.js

--- program2.js
+++ program2.js
@@ -3,8 +3,32 @@
  * @return {number}
  */
 var romanToInt = function(s) {
-    
+    const romanValues = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    };
+
+    let result = 0;
+    let prevValue = 0;
+
+    for (let i = s.length - 1; i >= 0; i--) {
+        const currValue = romanValues[s[i]];
+
+        if (currValue < prevValue) {
+            result -= currValue;
+        } else {
+            result += currValue;
+        }
+
+        prevValue = currValue;
+    }
+
+    return result;
 };
 
-
-module.exports={romanToInt}
+module.exports = { romanToInt };
